3c13fe0c3fc638b3699d4567789abc9156bccffb
John William <john.reyes@rappi.com>
2020-10-17 09:00:29 -0500

    reparaciones y ajustes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,17 +6,30 @@ import { ActualizarPiezaComponent } from './components/actualizar-pieza/actualiz
 import { RegistroAdminComponent } from './components/registrar-admin/registrar-admin.component';
 import { AgregarVehiculoComponent } from './pages/vehiculo/agregar-vehiculo/agregar-vehiculo.component';
 
-const routes: Routes = [
+// const routes: Routes = [
+//   { path: 'home', component: HomeComponent },
+//   { path: '', redirectTo: '/home', pathMatch: 'full' },
+//   { path: 'navbar', component: NavbarComponent },
+//   { path: 'pieza', component: ActualizarPiezaComponent },
+//   { path: 'registroAdmin', component: RegistroAdminComponent },
+//   { path: 'vehiculo', component: AgregarVehiculoComponent },
+// ];
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule],
+// })
+// export class AppRoutingModule {}
+const APP_ROUTES: Routes = [
+
   { path: 'home', component: HomeComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'navbar', component: NavbarComponent },
   { path: 'pieza', component: ActualizarPiezaComponent },
   { path: 'registroAdmin', component: RegistroAdminComponent },
   { path: 'vehiculo', component: AgregarVehiculoComponent },
+
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+
 ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-})
-export class AppRoutingModule {}
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
--- src/app/components/registrar-admin/registrar-admin.component.ts
+++ src/app/components/registrar-admin/registrar-admin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AdminService } from '../../services/admin.service';
 import { Admin } from '../../models/adminModel';
+import Swal from 'sweetalert2';
 
 
 
@@ -12,34 +13,44 @@ import { Admin } from '../../models/adminModel';
 export class RegistroAdminComponent implements OnInit {
 
 
-  public primerN: string;
+  public nombre: string;
   public apellido: string;
-  public email: string;
-  public password: string;
+  public telefono: string;
+  public correo: string;
+  public contrasenia: string;
   public nit: string;
-  public mostrar = false;
-  public empty = false;
+  
+
   
   constructor(private adminService: AdminService)
    {}
 
-   saveA( primerN: string, apellido: string, nit: string, email: string, password: string)
-   {
-    if (primerN == null || apellido == null || nit == null || email == null || password == null)
-  {
-    this.empty = true;
+   saveA( nombre: string, apellido: string, telefono:string, nit: string, correo: string, contrasenia: string){
 
-  }
-    else
-    {
-    let admin: Admin = {primerN , apellido, nit, email, password};
-    this.adminService.saveA(admin).subscribe((res: any) => {
-      console.log(res);
-      this.mostrar = res;
+    if (nombre == '' || apellido == '' || telefono == '' || nit == '' || correo == '' || contrasenia == ''){
+    Swal.fire({
+      icon: 'warning',
+      title: 'debe diligenciar todos los campos!',
+      showConfirmButton: true,
+      timer: 5000
 
+  });
+}else{
+
+    let admin: Admin = {nombre , apellido, nit, correo, contrasenia,telefono};
+    this.adminService.saveA(admin).subscribe((res: any) =>{
+      Swal.fire({
+        icon: 'success',
+        title: res.mensaje,
+        showConfirmButton: true,
+        timer: 30000
+      });
+      window.location.reload();
     });
   }
-   }
+}
+
+  
   ngOnInit(): void {}
 
 }
