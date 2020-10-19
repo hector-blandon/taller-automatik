0263e58d5a3c2de0999b21a54f2e571419a75a08
Hector Blandon <hlblandone@uqvirtual.edu.co>
2020-10-19 13:57:53 -0500

    Se organiza creacion de vehiculo

---
+++ src/app/models/vehiculo.model.ts
@@ -0,0 +1,3 @@
+export class Vehiculo {
+    constructor(public matricula: string = null, public marca: string = null, public color: string = null, public modelo: string = null) {}
+  }
--- src/app/pages/vehiculo/agregar-vehiculo/agregar-vehiculo.component.ts
+++ src/app/pages/vehiculo/agregar-vehiculo/agregar-vehiculo.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Vehiculo } from 'src/app/models/vehiculo.model';
+import { VehiculoService } from 'src/app/services/vehiculo.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-agregar-vehiculo',
@@ -6,7 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./agregar-vehiculo.component.css'],
 })
 export class AgregarVehiculoComponent implements OnInit {
-  constructor() {}
+  public matricula: string;
+  public marca: string;
+  public color: string;
+  public modelo: string;
+  constructor(public vehiculoService: VehiculoService) {}
 
   ngOnInit(): void {}
+  saveA( matricula: string, marca: string, color: string, modelo: string){
+
+    if (matricula === '' || marca === '' || color === '' || modelo === '') {
+      Swal.fire({
+        icon: 'warning',
+        title: 'debe diligenciar todos los campos!',
+        showConfirmButton: true,
+        timer: 5000
+    });
+    } else {
+    const vehiculo: Vehiculo = {matricula , marca, color, modelo};
+    this.vehiculoService.saveA(vehiculo).subscribe((res: any) => {
+      Swal.fire({
+        icon: 'success',
+        title: res.mensaje,
+        showConfirmButton: true,
+        timer: 30000
+      });
+      window.location.reload();
+    });
+    }
+  }
 }
