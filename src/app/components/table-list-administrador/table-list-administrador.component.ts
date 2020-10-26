e3a38da35348a1518d3a5e410956a79881cd09f5
Andrew Betancourt <afbetancourtg@gmail.com>
2020-10-26 14:20:04 -0500

    Interfaz Dashboard y tabla de Administradores

---
+++ src/app/components/table-list-administrador/table-list-administrador.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table-list-administrador',
+  templateUrl: './table-list-administrador.component.html',
+  styleUrls: ['./table-list-administrador.component.css']
+})
+export class TableListAdministradorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
