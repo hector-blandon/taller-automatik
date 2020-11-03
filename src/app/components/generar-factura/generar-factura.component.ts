f1e2c0438f504604870b69189e851a995ab23a28
Laura Daniela Florez Polania <ldflorezp@uqvirtual.edu.co>
2020-11-02 20:42:07 -0500

    se genera pdf de la plantilla de la factura

--- src/app/components/generar-factura/generar-factura.component.ts
+++ src/app/components/generar-factura/generar-factura.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-generar-factura',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GenerarFacturaComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit(): void {
   }
 
+ // tslint:disable-next-line:typedef
+ downloadPDF() {
+  // Extraemos el
+  const DATA = document.getElementById('htmlData');
+  const doc = new jsPDF('p', 'pt', 'a4');
+  const options = {
+    background: 'white',
+    scale: 3
+  };
+  html2canvas(DATA, options).then((canvas) => {
+
+    const img = canvas.toDataURL('image/PNG');
+
+    // Add image Canvas to PDF
+    const bufferX = 15;
+    const bufferY = 15;
+    const imgProps = (doc as any).getImageProperties(img);
+    const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+    const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+    doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
+    return doc;
+  }).then((docResult) => {
+    docResult.save(`${new Date().toISOString()}_.pdf`);
+  });
 }
+}
+
+
+
